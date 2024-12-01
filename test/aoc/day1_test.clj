2b4715a7f06d3f03dab53e6f75692e36670b283c
Stan Verberkt <2913270+verberktstan@users.noreply.github.com>
2024-12-01 23:13:34 +0100

    Update day1_test.clj

--- test/aoc/day1_test.clj
+++ test/aoc/day1_test.clj
@@ -4,14 +4,10 @@
    [aoc.core :as sut]
    [aoc.utils :as u]))
 
+(def props {:filename "resources/day1input.txt"})
+
 (deftest part-one
-  (is (= 2000468
-        (sut/distancex))))
+  (is (= 2000468 (sut/part-one props))))
   
-#_(deftest part-two
-  (is (= 18567089
-        (->> "resources/day1input.txt"
-          sut/read-lines
-          (map sut/parse-line)
-          sut/pivot-sort
-          sut/similarity))))
+(deftest part-two
+  (is (= 1856708 #_9 (sut/part-two props))))
