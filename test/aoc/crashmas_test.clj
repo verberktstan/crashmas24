2889681e6ac2496e082a7cd0dacbabad3322e5a1
Stan Verberkt <verberktstan@gmail.com>
2024-12-02 10:09:31 +0100

    Unify day1 and day2 test in one crashmas-test, rename resource files

--- test/aoc/crashmas_test.clj
+++ test/aoc/crashmas_test.clj
@@ -3,12 +3,10 @@
             [aoc.day1 :as day1]
             [aoc.day2 :as day2]))
 
-(deftest day1
-  (are [f result] (-> {:filename "resources/day1input.txt"} f (= result))
-    day1/part-one 2000468
-    day1/part-two 18567089))
+(deftest crashmas-test
+  (are [filename f result] (-> {:filename filename} f (= result))
+    "resources/day1.txt" day1/part-one 2000468
+    "resources/day1.txt" day1/part-two 18567089
 
-(deftest day2
-  (are [f result] (-> {:filename "resources/day2input.txt"} f (= result))
-    day2/part-one 359
-    day2/part-two 418))
+    "resources/day2.txt" day2/part-one 359
+    "resources/day2.txt" day2/part-two 418))
