caabe6080a8b43632797d6c5e615bb9e47c2eea8
Stan Verberkt <2913270+verberktstan@users.noreply.github.com>
2024-12-02 04:53:06 +0100

    Rename core to day1 (#3)
    * Rename core to day1
    
    * Update core.clj
    
    * Update day1_test.clj
    
    * Update day1.clj
    
    * Update day1.clj

---
+++ src/aoc/day1.clj
@@ -0,0 +1,37 @@
+(ns aoc.day1
+  (:require
+   [aoc.utils :as u]
+   [clojure.string :as str]
+   [clojure.edn :as edn]))
+
+(def parse-line (comp (partial mapv edn/read-string)
+                      #(str/split % #"\s+")))
+
+(def pivot (juxt (partial map first) (partial map second)))
+
+(def distance (comp abs -))
+
+(defn part-one [props]
+  (u/rearseduce
+    (assoc props :parser parse-line :reducer +)
+    pivot
+    (partial map sort)
+    (partial apply map distance)))
+
+(defn- location-lookup [right]
+  (fn [m location-id frequency]
+    (let [similarity (some-> location-id right (* location-id frequency))]
+      (cond-> m
+        similarity (update location-id (fnil + 0) similarity)))))
+
+(defn- similarity [coll]
+  (let [[left right] (map frequencies coll)]
+    (->> left
+         (reduce-kv (location-lookup right) nil)
+         vals)))
+
+(defn part-two [props]
+  (u/rearseduce
+    (assoc props :parser parse-line :reducer +)
+    pivot
+    similarity))
