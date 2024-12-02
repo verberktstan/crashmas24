4ea87136867bb9f53d0bae192b65c38e7a4d5f74
Stan Verberkt <2913270+verberktstan@users.noreply.github.com>
2024-12-02 07:06:51 +0100

    Update day1_test.clj

--- test/aoc/day1_test.clj
+++ test/aoc/day1_test.clj
@@ -1,8 +1,14 @@
 (ns aoc.day1-test
   (:require [clojure.test :refer [are deftest]]
-            [aoc.day1 :as day1]))
+            [aoc.day1 :as day1]
+            [aoc.day2 :as day2]))
 
 (deftest day1
   (are [f result] (-> {:filename "resources/day1input.txt"} f (= result))
     day1/part-one 2000468
     day1/part-two 18567089))
+
+(deftest day2
+  (are [f result] (-> {:filename "resources/day2input.txt"} f (= result))
+    day2/part-one 2000468
+    #_#_day2/part-two 18567089))
