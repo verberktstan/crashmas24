92e716f8b2ad5d9e3b339c521a76aa7cda05b3af
Stan Verberkt <2913270+verberktstan@users.noreply.github.com>
2024-12-04 19:17:12 +0100

    Update day2.clj

--- src/aoc/day2.clj
+++ src/aoc/day2.clj
@@ -1,6 +1,8 @@
 (ns aoc.day2
   (:require [aoc.utils :refer [transmute dissoc]]))
 
+(def day2 (constantly {:filename "day2.txt"}))
+
 ;; Day 2 - Part one; Find safe 'Red-Nosed Reactor' reports
 (defn- adjacent-levels [a b]
   (let [adjacent? (reduce into #{} [(some-> a (- 3) (range a))
@@ -10,10 +12,10 @@
 (defn- safe? [coll]
   (and (or (apply < coll) (apply > coll) nil) (reduce adjacent-levels coll) 1))
 
-(def part-one (transmute (partial keep safe?)))
+(def part-one (comp (transmute (partial keep safe?)) day2))
 
 ;; Day 2 - Part two; Find tolerated reports with the Problem Dampener
 (defn- problem-dampener [coll]
   (->> coll (map-indexed (dissoc coll)) (some safe?)))
 
-(def part-two (transmute (partial keep problem-dampener)))
+(def part-two (comp (transmute (partial keep problem-dampener)) day2))
