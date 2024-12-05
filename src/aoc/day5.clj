e289b62161654cfcafe6f07cc0513ed300ab928f
Stan Verberkt <2913270+verberktstan@users.noreply.github.com>
2024-12-06 00:27:18 +0100

    Create aoc.day5

---
+++ src/aoc/day5.clj
@@ -0,0 +1,10 @@
+(ns aoc.day5
+  (:require [aoc.utils :refer [transmute]]
+            [clojure.edn :as edn]
+            [clojure.string :as str]))
+
+;; Day 5 - Part one;
+
+(def part-one
+  (comp (transmute identity
+        (partial merge {:parser nil :reducer nil})))
