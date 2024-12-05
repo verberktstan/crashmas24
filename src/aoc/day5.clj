56e5a32efcf79ada6c58697aee10e55d6cae9998
Stan Verberkt <2913270+verberktstan@users.noreply.github.com>
2024-12-06 00:33:36 +0100

    Update day5.clj

--- src/aoc/day5.clj
+++ src/aoc/day5.clj
@@ -4,7 +4,20 @@
             [clojure.string :as str]))
 
 ;; Day 5 - Part one;
+(defn parse-line [s]
+  (when-let [coll (and (seq s) (str/split s #"\,|\|"))] 
+    (assoc nil (if (str/includes? s "|") :rule :update) (map edn/read-string coll))))
+
+(defn build-rules [coll]
+  (reduce
+    (fn [m {[a b :as rule] :rule update-coll :update}] 
+      (cond-> m
+        rule (update-in [:pre a] (comp set into) #{b}) 
+        rule (update-in [:post b] (comp set into) #{a}) 
+        update-coll (update :updates conj update-coll)))
+    nil
+    coll))
 
 (def part-one
-  (comp (transmute identity
-        (partial merge {:parser nil :reducer nil}))))
+  (comp (transmute build-rules
+        (partial merge {:parser parse-line :reducer nil}))))
